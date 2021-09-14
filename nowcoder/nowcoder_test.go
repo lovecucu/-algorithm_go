e9a73ef4d191eba9b023806efbaf05f38eae9a0d
yangding <yangding@bilibili.com>
2021-09-14 14:24:15 +0800

    add test code for spiralOrder

--- nowcoder/nowcoder_test.go
+++ nowcoder/nowcoder_test.go
@@ -494,4 +494,14 @@ func TestSpiralOrder(t *testing.T) {
 	if fmt.Sprint(spiralOrder(matrix)) != "[1 2 3 6 9 8 7 4 5]" {
 		t.Error(`TestSpiralOrder failed`)
 	}
+
+	matrix = [][]int{
+		{1, 2, 3, 4},
+		{5, 6, 7, 8},
+		{9, 10, 11, 12},
+		{13, 14, 15, 16},
+	}
+	if fmt.Sprint(spiralOrder(matrix)) != "[1 2 3 4 8 12 16 15 14 13 9 5 6 7 11 10]" {
+		t.Error(`TestSpiralOrder failed`)
+	}
 }
