707b8017dcc9234810eda0b63203672462ba4c6c
yangding <yangding@bilibili.com>
2021-10-02 11:51:11 +0800

    finish maxHeapInt priorityQueue

--- heap/heap_array.go
+++ heap/heap_array.go
@@ -1,5 +1,9 @@
 package heap
 
+import (
+	"container/heap"
+)
+
 func HeapSort(input []int) {
 	inputLen := len(input)
 	for i := 0; i < inputLen; i++ {
@@ -20,4 +24,77 @@ func minAjust(input []int) {
 			input[i], input[2*i+2] = input[2*i+2], input[i]
 		}
 	}
+}
+
+// 大顶堆
+type MaxHeapInt []int
+
+func (h MaxHeapInt) Len() int {
+	return len(h)
+}
+
+func (h MaxHeapInt) Less(i, j int) bool {
+	// 由于是最大堆，所以使用大于号
+	return h[i] > h[j]
+}
+
+func (h *MaxHeapInt) Swap(i, j int) {
+	(*h)[i], (*h)[j] = (*h)[j], (*h)[i]
+}
+
+func (h *MaxHeapInt) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+
+// Pop 弹出最后一个元素
+func (h *MaxHeapInt) Pop() interface{} {
+	res := (*h)[len(*h)-1]
+	*h = (*h)[:len(*h)-1]
+	return res
+}
+
+type Item struct {
+	value    string // The value of the item; arbitrary.
+	priority int    // The priority of the item in the queue.
+	// The index is needed by update and is maintained by the heap.Interface methods.
+	index int // The index of the item in the heap.
+}
+
+// A PriorityQueue implements heap.Interface and holds Items.
+type PriorityQueue []*Item
+
+func (pq PriorityQueue) Len() int { return len(pq) }
+
+func (pq PriorityQueue) Less(i, j int) bool {
+	// We want Pop to give us the highest, not lowest, priority so we use greater than here.
+	return pq[i].priority > pq[j].priority
+}
+
+func (pq PriorityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+	pq[i].index = i
+	pq[j].index = j
+}
+
+func (pq *PriorityQueue) Push(x interface{}) {
+	n := len(*pq)
+	item := x.(*Item)
+	item.index = n
+	*pq = append(*pq, item)
+}
+
+func (pq *PriorityQueue) Pop() interface{} {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	item.index = -1 // for safety
+	*pq = old[0 : n-1]
+	return item
+}
+
+// update modifies the priority and value of an Item in the queue.
+func (pq *PriorityQueue) update(item *Item, value string, priority int) {
+	item.value = value
+	item.priority = priority
+	heap.Fix(pq, item.index)
 }
--- heap/heap_array_test.go
+++ heap/heap_array_test.go
@@ -1,6 +1,7 @@
 package heap
 
 import (
+	"container/heap"
 	"fmt"
 	"testing"
 )
@@ -11,5 +12,57 @@ func TestHeapSort(t *testing.T) {
 	HeapSort(input)
 	if fmt.Sprint(input) != target {
 		t.Error(`TestHeapSort failed`, input)
+	}
+}
+
+func TestMaxHeapInt(t *testing.T) {
+	h := make(MaxHeapInt, 0)
+	heap.Init(&h)
+	heap.Push(&h, 2)
+	heap.Push(&h, 4)
+	heap.Push(&h, 5)
+	heap.Push(&h, 3)
+	heap.Push(&h, 1)
+	heap.Fix(&h, 6)
+
+	fmt.Println(heap.Pop(&h))
+	fmt.Println(heap.Pop(&h))
+	fmt.Println(heap.Pop(&h))
+	fmt.Println(heap.Pop(&h))
+	fmt.Println(heap.Pop(&h))
+}
+
+func TestPriorityQueue(t *testing.T) {
+	// Some items and their priorities.
+	items := map[string]int{
+		"banana": 3, "apple": 2, "pear": 4,
+	}
+
+	// Create a priority queue, put the items in it, and
+	// establish the priority queue (heap) invariants.
+	pq := make(PriorityQueue, len(items))
+	i := 0
+	for value, priority := range items {
+		pq[i] = &Item{
+			value:    value,
+			priority: priority,
+			index:    i,
+		}
+		i++
+	}
+	heap.Init(&pq)
+
+	// Insert a new item and then modify its priority.
+	item := &Item{
+		value:    "orange",
+		priority: 1,
+	}
+	heap.Push(&pq, item)
+	pq.update(item, item.value, 5)
+
+	// Take the items out; they arrive in decreasing priority order.
+	for pq.Len() > 0 {
+		item := heap.Pop(&pq).(*Item)
+		fmt.Printf("%.2d:%s \n", item.priority, item.value)
 	}
 }
