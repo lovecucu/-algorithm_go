65519e21939628c504635871f942681b4fb7a208
yangd <yangd@2345.com>
2021-11-15 20:34:56 +0800

    optimize rotate

--- crackingcodinginterview/1. arrays_and_strings.go
+++ crackingcodinginterview/1. arrays_and_strings.go
@@ -363,11 +363,7 @@ func rotate(matrix [][]int) [][]int {
 	n := len(matrix)
 	for i := 0; i < n/2; i++ {
 		for j := i; j < n-i-1; j++ {
-			tmp := matrix[i][j]
-			matrix[i][j] = matrix[n-j-1][i]
-			matrix[n-j-1][i] = matrix[n-i-1][n-j-1]
-			matrix[n-i-1][n-j-1] = matrix[j][n-i-1]
-			matrix[j][n-i-1] = tmp
+			matrix[i][j], matrix[n-j-1][i], matrix[n-i-1][n-j-1], matrix[j][n-i-1] = matrix[n-j-1][i], matrix[n-i-1][n-j-1], matrix[j][n-i-1], matrix[i][j]
 		}
 	}
 	return matrix
